228f885469bf2b9dfec6fbedeb6b4f094ef1bbcd
mana_kimura <m.i.l.l.wancan@gmail.com>
2017-09-06 07:03:14 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Web制作管理アプリへようこそ</h1>
+            {!! link_to_route('signup.get', 'ユーザ登録', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
